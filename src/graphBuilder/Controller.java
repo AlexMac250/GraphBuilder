e2d6f7b0f1bb904315e2c834307a40eec7bd4087
AlexMac250 <aleksandr@vasilenco.ru>
2018-07-12 17:21:51 +1000

    working version

--- src/graphBuilder/Controller.java
+++ src/graphBuilder/Controller.java
@@ -1,71 +1,80 @@
+/*
+    2*sqrt((-abs(abs(x)-1))*abs(3-abs(x))/((abs(x)-1)*(3-abs(x))))*(1+abs(abs(x)-3))*sqrt(1-pow2(x/7))+(5+0.97*(abs(x-0.5)+abs(x+0.5))-3*(abs(x-0.75)+abs(x+0.75)))*(1+abs(1-abs(x))/(1-abs(x)))
+    (-3)*sqrt(1-pow2(x/7))*sqrt(abs(abs(x)-4)/(abs(x)-4))
+
+
+    (2.71052+(1.5-0.5*abs(x))-1.35526*sqrt(4-pow2(abs(x)-1)))*sqrt(abs(abs(x)-1)/(abs(x)-1))+0.9
+*/
+
 package graphBuilder;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Lighting;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 import static graphBuilder.Main.*;
+import static graphBuilder.Calculator.*;
 
 
 public class Controller {
     public TextField funcField;
     public Label labelOfMessages;
     public Slider zoomSlider;
+    public BorderPane mainPane;
     @FXML Canvas mainCanvas;
 
-    private static GraphicsContext gc;
+    private static BorderPane mainPaneStat;
+
     public void initialize(){
-        gc = mainCanvas.getGraphicsContext2D();
+        mainPaneStat = mainPane;
     }
 
     public void clickBuild(){
+        if (funcField.getText().equals("")) return;
         String func = funcField.getText();
         chordsX.clear();
         chordsY.clear();
-        for (double i = -startendXY; i < startendXY; i+=delta) {
-            chordsX.add(i);
-            chordsY.add(/*FUNCTION*/ Math.sin(i));
-        }
-        labelOfMessages.setText("");
-        Main.draw(mainCanvas);
-        if (funcField.getText().equals("")) return;
-        List<String> expression = ExpressionParser.parse(funcField.getText());
-        boolean flag = ExpressionParser.flag;
-        if (flag) {
-            labelOfMessages.setText(funcField.getText()+" = "+String.valueOf(Ideone.calc(expression)));
-//            System.out.println();
-//            System.out.println(Ideone.calc(expression));
-        } else {
-            switch (ExpressionParser.error){
-                case bracketsNotMatched:
-                    labelOfMessages.setText("Скобки в заданном выражении не согласованы");
-                    break;
-                case incorrectExpression:
-                    labelOfMessages.setText("Некорректное выражение");
-                    break;
 
+        CharSequence value = "x";
+
+        for (double x = -startendXY; x < startendXY; x += delta) {
+            x *= 100;
+            x = Math.round(x);
+            x /= 100;
+            List<String> polsk = ExpressionParser.parse(func.replace(value,String.valueOf(x)));
+            if (!ExpressionParser.flag) {
+                switch (ExpressionParser.error){
+                    case bracketsNotMatched:
+                        showError("Скобки в заданном выражении не согласованы", "err");
+                        break;
+                    case incorrectExpression:
+                        showError("Некорректное выражение", "err");
+                        break;
+
+                }
+                ((Lighting) funcField.getEffect()).getLight().setColor(COLORS.wrong_color);
+                return;
             }
-            ((Lighting) funcField.getEffect()).getLight().setColor(COLORS.wrong_color);
+            if(!func.contains(value)) {
+                showError(func+" = "+Ideone.calc(ExpressionParser.parse(func)), "Пример решён)");
+                return;
+            }
+            double y = Ideone.calc(polsk);
+            chordsX.add(x);
+            chordsY.add(y);
         }
-        //gc.fillText(funcField.getText()+" = "+String.valueOf(Ideone.calc(ExpressionParser.parse(funcField.getText()))), 10, centerY*2-20);
         Main.draw(mainCanvas);
-    }
-
-    private String convert(List<String> postfix){
-        StringBuilder res = new StringBuilder();
-        for (String s : postfix) {
-            res.append(s);
-        }
-        return res.toString();
     }
 
     public void zoom() {
@@ -74,12 +83,50 @@ public class Controller {
         Main.draw(mainCanvas);
     }
 
-    public void showInfo(){
-        System.out.println("It works!");
+    static void showError(String message, String type){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Error setting native LAF: " + e);
+        }
+        mainPaneStat.setDisable(true);
+        JFrame frame = new JFrame();
+        final JDialog frMess = new JDialog(frame);
+        final JPanel panel = new JPanel();
+        final JPanel panel1 = new JPanel();
+        final JLabel text = new JLabel(message);
+        final JButton butOK = new JButton("Закрыть");
+        switch (type){
+            case "err":
+                frMess.setTitle("Ошибка!");
+                break;
+            case "msg":
+                frMess.setTitle("Сообщение");
+                break;
+            default:
+                frMess.setTitle(type);
+                break;
+        }
+        frMess.setResizable(false);
+        frMess.setAlwaysOnTop(true);
+        frMess.add(panel, BorderLayout.NORTH);
+        frMess.add(panel1, BorderLayout.SOUTH);
+        panel.add(text, BorderLayout.WEST);
+        panel1.add(butOK, BorderLayout.EAST);
+        frMess.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        butOK.addActionListener(e -> {
+            frame.dispose();
+            frMess.dispose();
+            mainPaneStat.setDisable(false);
+        });
+        frMess.pack();
+        frMess.setSize(new Dimension(frMess.getWidth()+70, frMess.getHeight()+20));
+        frMess.setLocationRelativeTo(null);
+        frMess.setVisible(true);
     }
 
-    double lxM;
-    double lyM;
+    private double lxM;
+    private double lyM;
     public void move(MouseEvent mouseEvent) {
         double x = mouseEvent.getScreenX();
         double y = mouseEvent.getScreenY();
@@ -108,7 +155,8 @@ public class Controller {
         ((Lighting) funcField.getEffect()).getLight().setColor(Color.valueOf("#ffffff"));
     }
 
-    double map(long x, double in_min, double in_max, long out_min, long out_max) {
+    @SuppressWarnings("SameParameterValue")
+    private double map(long x, double in_min, double in_max, long out_min, long out_max) {
         return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
     }
 
--- src/graphBuilder/Main.java
+++ src/graphBuilder/Main.java
@@ -1,25 +1,25 @@
 package graphBuilder;
 
+import java.util.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
-import java.util.*;
-
 public class Main extends Application {
 
-    static GraphicsContext gc;
-    Canvas canvas;
+    private static GraphicsContext gc;
+    private Canvas canvas;
     private static double canvasHeight;
     private static double canvasWidth;
     static double centerX;
     static double centerY;
-    static double delta = 0.1;
+    static double delta = 0.01;
     static double startendXY = 500;
     static int scale = 40;
 
@@ -28,11 +28,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Graph Builder by Aleksandr Vasilenko");
-        primaryStage.setScene(new Scene(root, 711, 600));
-        primaryStage.setMinWidth(711);
-        primaryStage.setMinHeight(600);
+        Parent root = FXMLLoader.load(getClass().getResource("resources/fxml/sample.fxml"));
+        primaryStage.setTitle("Graph Builder by Aleksandr Vasilenko БИС-17-02");
+        primaryStage.setScene(new Scene(root, 785, 635));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream( "resources/favicon.png" )));
+        primaryStage.setMinWidth(785);
+        primaryStage.setMinHeight(635);
         canvas = ((Canvas) root.lookup("#mainCanvas"));
         canvasWidth = canvas.getWidth();
         canvasHeight = canvas.getHeight();
@@ -60,31 +61,24 @@ public class Main extends Application {
         canvasHeight = canvas.getHeight();
         gc.setFill(COLORS.background);
         gc.fillRect(0,0,canvasWidth, canvasHeight);
-        gc.setFill(COLORS.btn1);
-        gc.setStroke(COLORS.btn1);
+        gc.setFill(COLORS.main_grid);
+        gc.setStroke(COLORS.main_grid);
         gc.setLineWidth(2);
         gc.setLineCap(StrokeLineCap.SQUARE);
-        gc.strokeLine(centerX,centerY + (-startendXY * scale) - 10,centerX,centerY + (startendXY * scale) + 5);
-        gc.strokeLine(centerX + (-startendXY * scale) - 5,centerY,centerX + (startendXY * scale) + 10, centerY);
-        gc.setLineWidth(2);
-
-        for (int i = (int) -startendXY; i < startendXY + 1;) {
-            if (scale < 6) i += 8;
-            else if (scale < 10) i += 4;
-            else if (scale < 20) i += 2;
-            else i++;
-            gc.strokeLine(centerX + (i*scale), centerY - 4, centerX + (i*scale), centerY+4);
-            gc.strokeLine(centerX - 4, centerY+(i*scale), centerX+4, centerY+(i*scale));
-            if (i != 0) gc.fillText(String.valueOf(i * (-1)), centerX + 12, centerY+(i * scale) + 5);
-            if (i == 0) gc.fillText(String.valueOf(i * (-1)), centerX + 5, centerY+(i * scale) + 15);
-            else gc.fillText(String.valueOf(i), centerX+(i*scale)-5, centerY + 20);
-        }
 
         if (scale > 35) {
             gc.setLineWidth(1);
             for (double i = -startendXY; i < startendXY + 1; i += 0.1) {
                 gc.strokeLine(centerX + (i * scale), centerY - 3, centerX + (i * scale), centerY + 3);
                 gc.strokeLine(centerX - 3, centerY + (i * scale), centerX + 3, centerY + (i * scale));
+                if (i != 0) {
+                    gc.setFill(COLORS.grid01);
+                    gc.setStroke(COLORS.grid01);
+                    gc.strokeLine(0, centerY + (i * scale), canvasWidth, centerY + (i * scale));           //   horizontal
+                    gc.strokeLine(centerX + (i * scale), 0, centerX + (i * scale), canvasHeight);           //   vertical
+                }
+                gc.setFill(COLORS.main_grid);
+                gc.setStroke(COLORS.main_grid);
                 if (scale > 129){
                     gc.strokeLine(centerX + (i * scale) - scale / 2, centerY - 3, centerX + (i * scale) - scale / 2, centerY + 3);
                     gc.strokeLine(centerX - 3, centerY + (i * scale), centerX + 3, centerY + (i * scale));
@@ -92,6 +86,38 @@ public class Main extends Application {
             }
         }
 
+        for (int i = (int) -startendXY; i < startendXY + 1;) {
+            if (scale < 6) i += 8;
+            else if (scale < 10) i += 4;
+            else if (scale < 20) i += 2;
+            else i++;
+
+            gc.setFill(COLORS.grid1);
+            gc.setStroke(COLORS.grid1);
+            gc.setLineWidth(2);
+
+            if (i != 0) {
+                if (scale > 35) gc.setLineWidth(2);
+                else gc.setLineWidth(1);
+                gc.strokeLine(centerX-centerX, centerY + (i * scale), canvasWidth, centerY + (i * scale));    //   horizontal
+                gc.strokeLine(centerX + (i * scale), centerY-centerY, centerX + (i * scale), canvasHeight);   //   vertical
+
+            }
+
+            gc.setFill(COLORS.main_grid);
+            gc.setStroke(COLORS.main_grid);
+            gc.setLineWidth(2);
+
+            gc.strokeLine(centerX + (i*scale), centerY - 4, centerX + (i*scale), centerY+4);   //   horizontal
+            gc.strokeLine(centerX - 4, centerY+(i*scale), centerX+4, centerY+(i*scale));       //   vertical
+
+            if (i != 0) gc.fillText(String.valueOf(i * (-1)), centerX + 12, centerY+(i * scale) + 5);
+            if (i == 0) gc.fillText(String.valueOf(i * (-1)), centerX + 5, centerY+(i * scale) + 15);
+            else gc.fillText(String.valueOf(i), centerX+(i*scale)-5, centerY + 20);
+        }
+
+        gc.strokeLine(centerX,centerY + (-startendXY * scale) - 10,centerX,centerY + (startendXY * scale) + 5);
+        gc.strokeLine(centerX + (-startendXY * scale) - 5,centerY,centerX + (startendXY * scale) + 10, centerY);
 
         if (chordsY.isEmpty() || chordsX.isEmpty()){
             return;
@@ -107,12 +133,13 @@ public class Main extends Application {
             lx = x;
             ly = y;
         }
+
     }
 
-    static void drawLineWithCords(Canvas canvas, double x1, double y1, double x2, double y2){
+    private static void drawLineWithCords(Canvas canvas, double x1, double y1, double x2, double y2){
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setLineWidth(1);
-        gc.setStroke(COLORS.main_color);
+        gc.setStroke(COLORS.function);
         gc.strokeLine(centerX + (x1 * scale), centerY -(y1 * scale), centerX + (x2 * scale), centerY - (y2 * scale));
     }
 
@@ -121,134 +148,4 @@ public class Main extends Application {
     }
 }
 
-class ExpressionParser {
 
-    enum ERROR { bracketsNotMatched, incorrectExpression }
-
-    private static String operators = "+-*/";
-    private static String delimiters = "() " + operators;
-    static boolean flag = true;
-    static ERROR error = null;
-
-    private static boolean isDelimiter(String token) {
-        if (token.length() != 1) return false;
-        for (int i = 0; i < delimiters.length(); i++) {
-            if (token.charAt(0) == delimiters.charAt(i)) return true;
-        }
-        return false;
-    }
-
-    private static boolean isOperator(String token) {
-        if (token.equals("u-")) return true;
-        for (int i = 0; i < operators.length(); i++) {
-            if (token.charAt(0) == operators.charAt(i)) return true;
-        }
-        return false;
-    }
-
-    private static boolean isFunction(String token) {
-        if (token.equals("sqrt") || token.equals("cube") || token.equals("pow2")) return true;
-        return false;
-    }
-
-    private static int priority(String token) {
-        if (token.equals("(")) return 1;
-        if (token.equals("+") || token.equals("-")) return 2;
-        if (token.equals("*") || token.equals("/")) return 3;
-        return 4;
-    }
-
-    public static List<String> parse(String infix) {
-        List<String> postfix = new ArrayList<>();
-        Deque<String> stack = new ArrayDeque<>();
-        StringTokenizer tokenizer = new StringTokenizer(infix, delimiters, true);
-        String prev = "";
-        String curr = "";
-        while (tokenizer.hasMoreTokens()) {
-            curr = tokenizer.nextToken();
-            if (!tokenizer.hasMoreTokens() && isOperator(curr)) {
-                System.out.println("Некорректное выражение.");
-                error = ERROR.incorrectExpression;
-                flag = false;
-                return postfix;
-            }
-            if (curr.equals(" ")) continue;
-            if (isFunction(curr)) stack.push(curr);
-            else if (isDelimiter(curr)) {
-                if (curr.equals("(")) stack.push(curr);
-                else if (curr.equals(")")) {
-                    while (!stack.peek().equals("(")) {
-                        postfix.add(stack.pop());
-                        if (stack.isEmpty()) {
-                            System.out.println("Скобки не согласованы.");
-                            error = ERROR.bracketsNotMatched;
-                            flag = false;
-                            return postfix;
-                        }
-                    }
-                    stack.pop();
-                    if (!stack.isEmpty() && isFunction(stack.peek())) {
-                        postfix.add(stack.pop());
-                    }
-                } else {
-                    if (curr.equals("-") && (prev.equals("") || (isDelimiter(prev)  && !prev.equals(")")))) {
-                        // унарный минус
-                        curr = "u-";
-                    } else {
-                        while (!stack.isEmpty() && (priority(curr) <= priority(stack.peek()))) {
-                            postfix.add(stack.pop());
-                        }
-
-                    }
-                    stack.push(curr);
-                }
-
-            }
-
-            else {
-                postfix.add(curr);
-            }
-            prev = curr;
-        }
-
-        while (!stack.isEmpty()) {
-            if (isOperator(stack.peek())) postfix.add(stack.pop());
-            else {
-                System.out.println("Скобки не согласованы.");
-                error = ERROR.bracketsNotMatched;
-                flag = false;
-                return postfix;
-            }
-        }
-        return postfix;
-    }
-}
-
-class Ideone {
-    public static Double calc(List<String> postfix) {
-        Deque<Double> stack = new ArrayDeque<Double>();
-        for (String x : postfix) {
-            System.out.print(x);
-            if (x.equals("sqrt")) stack.push(Math.sqrt(stack.pop()));
-            else if (x.equals("cube")) {
-                Double tmp = stack.pop();
-                stack.push(tmp * tmp * tmp);
-            }
-            else if (x.equals("pow2")) stack.push(Math.pow(2, stack.pop()));
-            else if (x.equals("+")) stack.push(stack.pop() + stack.pop());
-            else if (x.equals("-")) {
-                Double b = stack.pop(), a = stack.pop();
-                stack.push(a - b);
-            }
-            else if (x.equals("*")) stack.push(stack.pop() * stack.pop());
-            else if (x.equals("/")) {
-                Double b = stack.pop(), a = stack.pop();
-                stack.push(a / b);
-            }
-            else if (x.equals("u-")) stack.push(-stack.pop());
-            else if (x.equals("x")) stack.push(10.0);
-            else stack.push(Double.valueOf(x));
-        }
-        return stack.pop();
-    }
-}
